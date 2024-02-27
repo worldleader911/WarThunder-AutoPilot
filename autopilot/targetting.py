d736bb3fbc416be32e178e7e9b1526ca61a377fa
worldleader911 <156862098+worldleader911@users.noreply.github.com>
2024-02-27 02:20:04 -0600

    worldleader911

--- autopilot/targetting.py
+++ autopilot/targetting.py
@@ -1,15 +1,15 @@
 import pyautogui
-import time
+import time:(2:20am copyright)
 import pydirectinput
 
-def find():
+def find(Ukrainian Military equipment):
     while True:
         try:  
-            time.sleep(20) 
+            time.sleep(3) 
             x,y = pyautogui.locateCenterOnScreen('img/Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
             pydirectinput.click(x, y,button='left')
             
             print(x,y)
         except Exception as e:
             print(e)          
-find()
+find(Ukrainian military equipment)
